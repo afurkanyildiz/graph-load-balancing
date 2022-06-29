52af1c09c0c826509a5ebb64d683a9d34cbf7f12
harubyy <14926555+harubyy@users.noreply.github.com>
2022-06-29 23:29:08 +0300

    bug fix

--- code/extract_feature.sh
+++ code/extract_feature.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "--help" ] || [ "$#" -ne 1 ]
 then
   echo "\$1: pattern to look for in all the reports"
+  exit
 fi
 
 while read matrix
@@ -11,4 +12,4 @@ do
   echo "matrix: $name"
   grep "$1" report."$name"
   echo  ""
-done < matrix.ID.name
+done < matrix.ID.name.BIG
--- code/extract_features.sh
+++ code/extract_features.sh
@@ -7,10 +7,10 @@ echo  "matrix,failed indegree, failed row cost, failed level size" >> failed.csv
 echo -e "\nmatrix,# of rewritten rows"  >> rewritten.csv
 ./extract_feature.sh "rewritten rows" | cut -d: -f2 | sed -e '/^$/d' | paste - - -d,  >> rewritten.csv
 
+set -x
 echo  -e "\nmatrix, # of levels after rewrite"  >> numOfLevels.csv
-matrix=`./extract_feature.sh "num. of levels"  | head -1 | cut -d: -f2`
-levels=`./extract_feature.sh "num. of levels"  |  sed -e '/^$/d' | tail -1 | cut -d, -f2`
-echo -e "\n$matrix,$levels" >> numOfLevels.csv
+./extract_feature.sh "num. of levels" | sed -e '0~2d' | cut -d: -f2 | paste - - -d' ' >> numOfLevels.csv
+set +x
 
 cat failed.csv rewritten.csv numOfLevels.csv >> features.csv
 
